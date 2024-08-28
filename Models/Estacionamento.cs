7d645f370f11f0ed417eebca4cefd5e68663b47c
juanvformoso <juanformoso55@gmail.com>
2024-08-27 22:49:06 -0400

    feat: :sparkles: Atualiza Models/Estacionamento.cs.
    Implementa função RemoverVeiculo.

--- Models/Estacionamento.cs
+++ Models/Estacionamento.cs
@@ -32,20 +32,22 @@ namespace ParkingSystem.Models
         public void RemoverVeiculo()
         {
             Console.WriteLine("Digite a placa do veículo para remover:");
-            // TODO: Pedir para o usuário digitar a placa e armazenar na variável placa
-            string placa = "";
+            string placa = Console.ReadLine()?.ToUpper();
 
             // Verifica se o veículo existe
-            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            if (veiculos.Any(x => x == placa))
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
-                /* TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                e realizar o cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal.*/
-                int horas = 0;
-                decimal valorTotal = 0;
-
-                // TODO: Remover a placa digitada da lista de veículos
-                Console.WriteLine($"O veículo {placa} foi removido e o preço total é de: R$ {valorTotal}");
+                if (int.TryParse(Console.ReadLine(), out int horas))
+                {
+                    decimal valorTotal = precoInicial + precoPorHora * horas;
+                    veiculos.Remove(placa);
+                    Console.WriteLine($"O veículo {placa} foi removido e o preço total é de R$ {valorTotal:F2}");
+                }
+                else
+                {
+                    Console.WriteLine("Quantidade de horas inválida. Tente novamente.");
+                }
             }
             else
             {
